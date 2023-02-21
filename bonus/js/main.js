ebaf4715abf6b00396be406001f3d43ad03298a2
leonardoserra <serra.leonardo92@gmail.com>
2023-02-21 14:14:14 +0100

    aggiunto il 23 su js come da consegna

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -6,6 +6,6 @@ const userColor = prompt("Ci siamo quasi! Dimmi il tuo colore preferito!");
 
 // console.log(userName, userSurname, userColor);
 
-document.getElementById("gen-pwd").innerHTML = userName + userSurname + userColor;
+document.getElementById("gen-pwd").innerHTML = userName + userSurname + userColor + 23;
 document.getElementById("wow").innerHTML = "WoooW";
 document.getElementById("suggestion").innerHTML = "PRO TIP: usalo su tutti i tuoi account, 10/10 Best Hacker Magazine";
