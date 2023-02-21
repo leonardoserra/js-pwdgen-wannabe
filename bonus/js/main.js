7211d90a63bfdfcd5b147f5465fdb7cd3c8f927b
leonardoserra <serra.leonardo92@gmail.com>
2023-02-21 14:10:01 +0100

    ok bonus completo

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -7,5 +7,5 @@ const userColor = prompt("Ci siamo quasi! Dimmi il tuo colore preferito!");
 // console.log(userName, userSurname, userColor);
 
 document.getElementById("gen-pwd").innerHTML = userName + userSurname + userColor;
-
+document.getElementById("wow").innerHTML = "WoooW";
 document.getElementById("suggestion").innerHTML = "PRO TIP: usalo su tutti i tuoi account, 10/10 Best Hacker Magazine";
