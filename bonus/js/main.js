cb8b1bed9bc644527adc07411d2f91da07eb10a3
leonardoserra <serra.leonardo92@gmail.com>
2023-02-21 13:19:41 +0100

    creo cartella bonus per stile

---
+++ bonus/js/main.js
@@ -0,0 +1,9 @@
+const userName = prompt("Ciao creiamo la tua password! Dimmi come ti chiami");
+// console.log(userName);
+
+const userSurname = prompt("Adesso dimmi il tuo cognome");
+const userColor = prompt("Ci siamo quasi! Dimmi il tuo colore preferito!");
+
+// console.log(userName, userSurname, userColor);
+
+document.getElementById("gen-pwd").innerHTML = userName + userSurname + userColor;
